979019b49728ee5bcf3286cc83c07ece6ddedb6c
Quan Pham <doraemonfanclub@gmail.com>
2022-10-26 02:33:31 +0700

    Add LICENSE

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # React 3D hover card
 
 Library to create 3D hover card effect (like Steam trading card)
+
+```
+npm install @darenft/react-3d-hover-card
+```
+
+then use it like this:
+
+```tsx
+function App() {
+  return (
+    <div className="App">
+      <SteamCard scaleFactor={1.4}>
+        <img src="https://s3.duellinksmeta.com/cards/60c2b3aba0e24f2d54a52a69_w360.webp" />
+      </SteamCard>
+    </div>
+  );
+}
+```
--- src/app.tsx
+++ src/app.tsx
@@ -6,12 +6,7 @@ function App() {
   return (
     <div className="App">
       <SteamCard scaleFactor={1.4}>
-        <img
-          src="https://s3.duellinksmeta.com/cards/60c2b3aba0e24f2d54a52a69_w360.webp"
-          style={{
-            transition: "all 250ms ease-out",
-          }}
-        />
+        <img src="https://s3.duellinksmeta.com/cards/60c2b3aba0e24f2d54a52a69_w360.webp" />
       </SteamCard>
     </div>
   );
